ed4569c5ece1a5e163cec616cdf320703fd4e989
Gayathri0209 <gayathrigundaboina@gmail.com>
2023-05-11 18:17:00 +0530

    first commit

---
+++ js/questions.js
@@ -0,0 +1,171 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What does HTML stand for?",
+  answer: "Hyper Text Markup Language",
+  options: [
+    "Hyper Text Preprocessor",
+    "Hyper Text Markup Language",
+    "Hyper Text Multiple Language",
+    "Hyper Tool Multi Language"
+  ]
+},
+  {
+    numb: 2,
+    question: " The correct sequence of HTML tags for starting a webpage is -" ,
+    answer:  "HTML, Head, Title, Body" ,
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML,Title,Head,Body"
+    ]
+},
+  {
+    numb: 3,
+    question: "To create HTML page, you need _____" ,
+    answer:   "Both Web browser and text editor",
+    options: [
+      "Web browser",
+      "text editor",
+      "Both Web browser and text editor",
+      "None of the above"
+    ]
+},
+  {
+    numb: 4,
+    question: "What does the “&lt; br &gt;” tag add to your webpage?",
+    answer:  "Line break",
+    options: [
+      "Long break",
+      "Paragraph break",
+      "Line break",
+      "None of the above"
+    ]
+},
+  {
+    numb: 5,
+    question: "An HTML program is saved by using the ____ extension. " ,
+    answer:  ".html",
+    options: [
+      ".ht",
+      ".html",
+      ".hml",
+      "None of the above"
+    ]
+},
+  {
+    numb: 6,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+},
+  {
+    numb:7,
+    question:"The property in CSS used to change the background color of an element is-",
+    answer:"background-color",
+    options:[
+      "bgcolor",
+      "color",
+      "background-color",
+      "All of the above"
+    ]
+
+},
+  {
+    numb:8,
+    question:"The CSS property used to control the element's font-size is-",
+    answer:"font-size",
+    options:[
+      "text-style",
+      "text-size",
+      "font-size",
+      "None of the above"
+    ]
+
+},
+  {
+    numb:9,
+    question:"The HTML attribute used to define the inline styles is -",
+    answer:"style",
+    options:[
+      "style",
+      "styles",
+      "class",
+      "None of the above"
+    ]
+
+},
+  {
+    numb:10,
+    question:"Which of the following CSS property is used to add shadows to the text?",
+    answer:"text-shadow",
+    options:[
+      "text-shadow",
+      "text-stroke",
+      "text-overflow",
+     " text-decoration"
+    ]
+
+},
+  {
+    numb: 11,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+},
+  {
+    numb: 12,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+},
+  {
+    numb: 13,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+},
+{
+    numb: 14,
+    question:" Which type of JavaScript language is __ ",
+    answer:"Object-Based",
+    options: [
+      "Object-Oriented",
+      "Object-Based",
+      "Assembly-language",
+      "High-level"
+    ]
+},
+  {
+    numb:15,
+    question:"Which of the following keywords is used to define a variable in Javascript?",
+    answer:"Both var and let",
+    options: [
+      "var",
+      "let",
+      "Both var and let",
+      "None of the above"
+    ]
+}
+]
